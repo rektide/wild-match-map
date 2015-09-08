ff59dbba9e017cf1d94c373b7376e9f822cf4cf0
rektide <rektide@voodoowarez.com>
2015-09-07 21:12:15 -0400

    Implement other Map functions. Note of hazard on set.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Wild Match Map is a "rendezvous" Map where keys used for get and set may have wi
 This library was created in radical opposition to the principles of concealment that both EventEmitter and EventTarget enact: these libraries explicitly prevent knowing the bindings, EventEmitter offering to describe listeners but not not emitters, and EventTarget refusing to admit both listeners and emitters.
 
 In comparison, Wild Match Map permits arbitrary wildcard bindings. Although not explicitly a event emitter- instead a general Map- the Map is designed to allow explorative bindings.
+
+# Recommended Use
+
+The normal mode to use Wild Match Map is with a {generator:function(){}} provided, that creates objects on demand when a `get` is issued. `set`, by comparison, has the potential to set unintended matches.
--- wild-match-map.js
+++ wild-match-map.js
@@ -1,44 +1,74 @@
 function WildMatchMap(opts){
+	if(opts){
+		if(opts.generator){
+			var generator= opts.generator
+			Object.defineProperty(this, "_generator", {
+				get: function(){
+					return generator
+				}
+			})
+		}
+	}
 }
 
 WildMatchMap.prototype.get= function get(key){
-	var getter = WildMatchMap.getter(key)
-	return getter.call(this)
+	var got= getter(key, !!this._generator),
+	  matches= getter.call(this)
+	return Object.keys(matches)
 }
 
 WildMatchMap.prototype.set= function set(key, value){
-	
+	var got= getter(key, null, value),
+	  matches= getter.call(this)
 }
 
-/**
-  Find all leaves
-*/
-WildMatchMap.getter= function(keys, create){
+function getter(keys, create, set){
 	keys= parse(keys)
 	return function(){
-		var cursors= [this]
-		for(var key of keys){
-			var next= []
+		var cursors= [this],
+		  assign= false
+		for(var i= 0; i< keys.length; ++i){
+			if( set&& i+ 1=== keys.length){
+				assign= true
+			}
+			var key= keys[i],
+			  next= []
 			for(var i= 0; i< cursors.length; ++i){
+				if( assign){
+					var old= cursors[key]
+					cursors[key]= set
+					continue
+				}
 				var el= cursors[i],
 				  matches
 				for(var slot in el){
+					// find all matching keys for this cursor
 					matches= findMatches(slot, [key])
 				}
-				if( matches|| create&& !matches){
+				if( matches|| create|| set){
 					if(!matches){
-						cursor[key]= {}
-						matches= {key: true}
+						// placeholder
+						matches= {}
+					}
+					if( create && !matches[key]){
+						// create our match
+						cursor[key]= this._generator ? this._generator() : {}
+						matches[key]= true
 					}
 					for(var j in matches){
-						var match= cursor[ matches[ j]]
-						next.push(match)
+						if( !set){
+							// get the found slot
+							var match= cursor[ j]
+							next.push( match)
+						}else{
+							cursor[ j]= set
+						}
 					}
 				}
 			}
 			cursors= next
 		}
-		return matches
+		return currsors
 	}
 }
 
@@ -144,5 +174,49 @@ function parse(keyString, sep){
 	var keys= keyString.split(sep)
 	return keys
 }
+
+Object.defineProperty(WildMatchMap.prototype, "size", {
+	get: function(){
+		return Object.keys(this).length
+	}
+})
+
+WildMatchMap.prototype.clear= function(){
+	for(var key in this){
+		this.delete(key)
+	}
+}
+
+WildMatchMap.prototype.delete= function(key){
+	delete this[key]
+}
+
+WildMatchMap.prototype.entries= function(){
+	return Object.keys(this).map(function(val, key){
+		return [key, val]
+	})
+}
+
+WildMatchMap.prototype.forEach= function(fn, thisArg){
+}
+
+WildMatchMap.prototype.has= function(key){
+	return this[key] !== undefined
+}
+
+WildMatchMap.prototype.keys= function(){
+	return Object.keys(this)
+}
+
+WildMatchMap.prototype.values= function(){
+	return Object.keys(this).map(function(val){return val})
+}
+
+Object.defineProperty(WildMatchMap, Symbol.species, {
+	get: function(){
+		return WildMatchMap
+	}
+})
+
 
 module.exports= WildMatchMap
